8874497c6a9f644fbfc97105e35f9decb31c894d
Saurabh K Saralaya <s97work@gmail.com>
2018-05-26 01:41:19 +0530

    Inital Setup

---
+++ Assets/Scripts/Player_Animations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//
+using UnityEngine.Networking;
+
+namespace MFramework
+{
+
+	public class Player_Animations : NetworkBehaviour 
+	{
+
+		public Animator playerAnimator;
+		
+		void Update ()
+		{
+			CheckForPlayerInput();
+		}
+
+		void CheckForPlayerInput()
+		{
+			if (!isLocalPlayer)
+			{
+				return;
+			}
+
+			if (Mathf.Abs( Input.GetAxis("Vertical")) > 0
+			|| Mathf.Abs( Input.GetAxis("Horizontal")) > 0)
+			{
+				playerAnimator.SetBool("Moving", true);
+			}
+
+			else {
+				playerAnimator.SetBool("Moving", false);
+			}
+		}
+	}	
+}
